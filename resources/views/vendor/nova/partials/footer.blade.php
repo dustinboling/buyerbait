d9dedd92ffea6b40ed5c300b380092e3c9aaa90d
Dustin Boling <dustin@dustinboling.com>
2019-04-25 16:48:44 -0700

    updates developer information

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,9 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+    <a href="https://hello.eugenerealtygroup.com/" class="text-primary dim no-underline">HelloBuyer App</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
+    &copy; {{ date('Y') }} Eugene Realty Group LLC - By Dustin Boling
+    <span class="px-1">&middot;</span>
+    Questions? (541) 999-1884
     <span class="px-1">&middot;</span>
     v{{ Laravel\Nova\Nova::version() }}
 </p>
